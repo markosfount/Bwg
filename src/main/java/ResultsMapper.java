d5d7a2dc475711256b9a8b2bab5d1d758104a3c7
markosfount <sc.markos@gmail.com>
2018-01-21 23:02:50 +0100

    more parsing and mapping

--- src/main/java/ResultsMapper.java
+++ src/main/java/ResultsMapper.java
@@ -2,9 +2,18 @@ import model.WgResult;
 import org.jsoup.nodes.Element;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class ResultsMapper {
+
+    private static final Pattern EURO = Pattern.compile("€", Pattern.LITERAL);
+    private static final Pattern LIST_DETAILS = Pattern.compile("liste-details-ad-", Pattern.LITERAL);
+    private static final Pattern SIZE_PRICE_SPLIT = Pattern.compile("Pattern.compile(\"m² -\")", Pattern.LITERAL);
+    private static final String ID = "id";
+    private static final String SIZE_PRICE_WR = ".detail-size-price-wrapper";
+    private static final String DETAIL_AN = ".detailansicht";
 
     public List<WgResult> mapResults(List<Element> elements) {
         return elements.stream()
@@ -13,8 +22,18 @@ public class ResultsMapper {
     }
 
     private WgResult mapResult(Element element) {
+        String extId = LIST_DETAILS.split(element.attr(ID))[1];
 
-        return WgResult.builder().build();
+        String[] priceAndSize = SIZE_PRICE_SPLIT.split(element.select(SIZE_PRICE_WR).select(DETAIL_AN).text());
+        String price = priceAndSize[0];
+        String size = EURO.matcher(priceAndSize[0]).replaceAll(Matcher.quoteReplacement(""));
+
+
+        return WgResult.builder()
+                .extId(extId)
+                .size(size)
+                .price(price)
+                .build();
     }
 
 }
--- src/main/java/model/WgResult.java
+++ src/main/java/model/WgResult.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 
 @Builder
 public class WgResult {
-
+    private String extId;
+    private String price;
+    private String size;
 
 }
