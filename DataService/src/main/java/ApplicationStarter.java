1d59fbd16e230d60737b2bdaab75799f700ca0e8
markosfount <sc.markos@gmail.com>
2018-02-20 23:16:05 +0100

    fix spring boot configuration

--- DataService/src/main/java/ApplicationStarter.java
+++ DataService/src/main/java/ApplicationStarter.java
@@ -1,11 +1,7 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+//@SpringBootApplication
 public class ApplicationStarter {
 
-    public static void main(String... args) {
-        SpringApplication.run(ApplicationStarter.class, args);
-    }
+//    public static void main(String... args) {
+//        SpringApplication.run(ApplicationStarter.class, args);
+//    }
 }
-
--- DataService/src/main/java/DataServiceController.java
+++ DataService/src/main/java/DataServiceController.java
@@ -1,10 +1,20 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAutoConfiguration
 public class DataServiceController {
 
-        @RequestMapping("/")
-        public String index() {
-            return "Greetings from Spring Boot!";
-        }
+    @RequestMapping("/insert")
+    public String insertData() {
+        return "data inserted \n";
+    }
+
+    public static void main(String... args) throws Exception {
+        SpringApplication.run(DataServiceController.class, args);
+    }
+
 
 }
